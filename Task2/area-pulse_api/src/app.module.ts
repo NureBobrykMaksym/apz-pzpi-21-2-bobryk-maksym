e146daacfee59c1892f372a80df415fd3b749cf9
Bobryk Maksym <maksym.bobryk@nure.ua>
2024-05-01 21:35:41 +0300

    feat: add jwt auth

--- Task2/area-pulse_api/src/app.module.ts
+++ Task2/area-pulse_api/src/app.module.ts
@@ -1,13 +1,28 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { dataSourceOptions } from './db/data-source';
 import { LocationModule } from './location/location.module';
+import { UserModule } from './user/user.module';
+import { AuthMiddleware } from './user/middlewares/auth.middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LocationModule, TypeOrmModule.forRoot(dataSourceOptions)],
+  imports: [
+    LocationModule,
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UserModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
