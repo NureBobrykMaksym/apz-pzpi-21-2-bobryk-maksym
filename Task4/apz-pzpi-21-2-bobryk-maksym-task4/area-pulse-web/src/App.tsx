f091cf86f7ac37dfa8b798dd1782defaa35c3fe7
Bobryk Maksym <maksym.bobryk@nure.ua>
2024-05-22 17:57:32 +0300

    feat: react query setup

--- Task4/apz-pzpi-21-2-bobryk-maksym-task4/area-pulse-web/src/App.tsx
+++ Task4/apz-pzpi-21-2-bobryk-maksym-task4/area-pulse-web/src/App.tsx
@@ -1,19 +1,31 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { RootLayout } from './components/layouts/RootLayout';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RootLayout />} errorElement={<div>404</div>}>
-          <Route index element={<div>homepage</div>} />
-          {/* <Route path="sign-up" element={<SignUp />} /> */}
-          {/* <Route path="login" element={<Login />} /> */}
-          <Route path="*" element={<div>404</div>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<RootLayout />}
+            errorElement={<div>404</div>}
+          >
+            <Route index element={<div>homepage</div>} />
+            {/* <Route path="sign-up" element={<SignUp />} /> */}
+            {/* <Route path="login" element={<Login />} /> */}
+            <Route path="*" element={<div>404</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 }
 
